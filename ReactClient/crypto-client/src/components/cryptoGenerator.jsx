f6b26694be88119c52dd30de626f32794780fbdf
sharif0rifat <sharif.rifat1986@gmail.com>
2020-03-30 12:27:58 +0600

    Botstrap added and design changed

--- ReactClient/crypto-client/src/components/cryptoGenerator.jsx
+++ ReactClient/crypto-client/src/components/cryptoGenerator.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 class CryptoGeneraotr extends Component {
   // state = {  }
   render() {
     return (
       <div>
-        <span>Number of Crypto Seriel</span>
-        <input type="text" />
-        <button>Generate</button>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <span className="badge badge-primary">
+                Number of Crypto Seriel
+              </span>
+              <input className="form-control" type="text" />
+              <button className="btn btn-primary">Generate</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
